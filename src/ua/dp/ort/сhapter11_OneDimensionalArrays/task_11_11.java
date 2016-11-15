586e329df55450bac9e62f48ed79c5bfdbb95785
StellaVS <StalinaVS23@gmail.com>
2016-11-15 23:04:13 +0200

    added System.nanoTime()

--- src/ua/dp/ort/сhapter11_OneDimensionalArrays/task_11_11.java
+++ src/ua/dp/ort/сhapter11_OneDimensionalArrays/task_11_11.java
@@ -2,16 +2,17 @@ package ua.dp.ort.сhapter11_OneDimensionalArrays;
 
 import java.util.Random;
 
-public class Task_11_11 {
+public class task_11_11 {
 	public static void main(String[] args) {
 		// 11.11.* Используя датчик случайных чисел, заполнить массив из
 		// двадцати элемен-тов неповторяющимися числами.
 		int[] dimenArray = new int[20];
-		Random random = new Random();
+		Random random = new Random(System.nanoTime());
 		for (int i = 0; i < dimenArray.length; ++i) {
-			int compareA = random.nextInt(1) + i;
-			dimenArray[i] = compareA;
-			System.out.print(dimenArray[i] + "; ");
+			int compareA = (random.nextInt(100)*(i+1));
+					dimenArray[i] = compareA;
+					System.out.print(dimenArray[i] + "; ");
+				}
+			
 		}
 	}
-}

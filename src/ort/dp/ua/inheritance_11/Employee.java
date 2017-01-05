862ddabbac36a4f1529c977f549944a6b47f8408
StellaVS <StalinaVS23@gmail.com>
2017-01-05 06:45:04 +0200

    Task Over

--- src/ort/dp/ua/inheritance_11/Employee.java
+++ src/ort/dp/ua/inheritance_11/Employee.java
@@ -1,14 +1,16 @@
 package ort.dp.ua.inheritance_11;
 
-abstract class Employee {
+abstract class Employee implements Comparable<Employee> {
 	private String _name;
+	private int _rate;
+	private String _persNumber;
 
-	public Employee(String name, int rate) {
+	public Employee(String name, int rate, String persNumber) {
+
 		_name = name;
 		_rate = rate;
+		_persNumber = persNumber;
 	}
-
-	private int _rate;
 
 	public String getName() {
 		return _name;
@@ -22,9 +24,29 @@ abstract class Employee {
 		return _rate;
 	}
 
+	public String getPersNumber() {
+		return _persNumber;
+	}
+
+	public void setPersNumber(String _persNumber) {
+		this._persNumber = _persNumber;
+	}
+
 	public void setRate(int rate) {
 		_rate = rate;
 	}
 
 	abstract int calcAvgMonthlySalary();
+
+	@Override
+	public int compareTo (Employee obj) {
+		Employee result = (Employee) obj;
+		if (this.calcAvgMonthlySalary() < result.calcAvgMonthlySalary()) {
+			return 1;
+		} else if (this.calcAvgMonthlySalary() > result.calcAvgMonthlySalary()) {
+			return -1;
+		}
+		return _name.compareTo(result._name);
+	}
+
 }
--- src/ort/dp/ua/inheritance_11/MainEmployee.java
+++ src/ort/dp/ua/inheritance_11/MainEmployee.java
@@ -1,17 +1,50 @@
 package ort.dp.ua.inheritance_11;
 
+import java.util.Arrays;
+
 public class MainEmployee {
-
+/*	11. Построить три класса (базовый и 2 потомка), описывающих некоторых работников с почасовой оплатой (один из потомков) и фиксированной оплатой (второй потомок). Описать в базовом классе абстрактный метод для расчета среднемесячной заработной платы. Для «повременщиков» формула для расчета такова: «среднемесячная заработная плата = 20.8 * 8 * почасовую ставку», для работников с фиксированной оплатой «среднемесячная заработная плата = фиксированной месячной оплате».
+			a) Упорядочить всю последовательность работников по убыванию среднемесячного заработка. При совпадении зарплаты – упорядочивать данные по алфавиту по имени. Вывести идентификатор работника, имя и среднемесячный заработок для всех элементов списка.
+			b) Вывести первые 5 имен работников из полученного в пункте а) списка.
+			c) Вывести последние 3 идентификатора работников из полученного в пункте а) списка.
+*/
 	public static void main(String[] args) {
-		Employee[] emp = new Employee[4];
-		emp[0] = new EmployeeFixedRate("Ivan", 123);
-		emp[1] = new EmployeeHourlyPayment("Vasya", 10);
-		emp[2] = new EmployeeHourlyPayment("Zoya", 5);
-		emp[3] = new EmployeeFixedRate("Irra", 5555);
+		int index = 0;
+		Employee[] employeeM = new Employee[6];
+		employeeM[0] = new EmployeeFixedRate("Ivan", 5555, "01257866");
+		employeeM[1] = new EmployeeHourlyPayment("Vasya", 10, "01257863");
+		employeeM[2] = new EmployeeHourlyPayment("Zoya", 5, "01257869");
+		employeeM[3] = new EmployeeFixedRate("Cifra", 5555, "01257868");
+		employeeM[4] = new EmployeeFixedRate("Andy", 5555, "01257877");
+		employeeM[5] = new EmployeeFixedRate("Fedor", 5555, "01257958");
 
-		for (int i = 0; i < emp.length; ++i) {
-			System.out.println(emp[i].getName() + " - " + emp[i].calcAvgMonthlySalary());
+		Arrays.sort(employeeM);
+		System.out
+				.println("a) Упорядочить всю последовательность работников по убыванию среднемесячного заработка. "
+						+ "При совпадении зарплаты – упорядочивать данные по алфавиту по имени. Вывести идентификатор работника, "
+						+ "имя и среднемесячный заработок для всех элементов списка.");
+
+		for (Employee i : employeeM) {
+			System.out.println(i.getPersNumber() + " - " + i.getName() + " - "
+					+ i.calcAvgMonthlySalary());
 		}
+		System.out
+				.println("b) Вывести первые 5 имен работников из полученного в пункте а) списка.");
+		for (Employee i : employeeM) {
+			index++;
+			System.out.println(i.getName());
+			if (index == 5)
+				break;
+		}
+	
+		System.out
+		.println("c) Вывести последние 3 идентификатора работников из полученного в пункте а) списка.");
+	for (int i = employeeM.length-3; i < employeeM.length; ++i) {
+		System.out
+				.println(employeeM[i].getPersNumber());
 	}
 
-}
+}}
+
+
+

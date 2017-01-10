8ff886ab4e3ccf2743a3a2400773c10b102491ba
StellaVS <StalinaVS23@gmail.com>
2017-01-10 21:55:28 +0200

    LinkedList is ready

--- src/dp/ort/ua_OOP_LinkedList_08/ElementList.java
+++ src/dp/ort/ua_OOP_LinkedList_08/ElementList.java
@@ -1,10 +1,34 @@
 package dp.ort.ua_OOP_LinkedList_08;
 
 public class ElementList<O> {
-	ElementList<O> _next;
-	O _value;
+
+	private ElementList _next;
+	private O _value;
 
 	public ElementList(O value) {
 		_value = value;
+		_next = null;
 	}
+
+	public ElementList(O value, ElementList next) {
+		_next = next;
+		_value = value;
+	}
+
+	public ElementList getNext() {
+		return _next;
+	}
+
+	public void setNext(ElementList next) {
+		_next = next;
+	}
+
+	public O getValue() {
+		return _value;
+	}
+
+	public void setValue(O value) {
+		_value = value;
+	}
+
 }
--- src/dp/ort/ua_OOP_LinkedList_08/ImplementLinkedList.java
+++ src/dp/ort/ua_OOP_LinkedList_08/ImplementLinkedList.java
@@ -1,48 +1,73 @@
 package dp.ort.ua_OOP_LinkedList_08;
 /*8. Односвязным списком называют набор элементов, каждый из которых 
  * содержит собственно полезные данные, и ссылку на следующий элемент.
- *  Создать такой список и реализовать для него CRUD операции*/
+ *  Создать такой список и реализовать для него CRUD операции
+ **/
 
 public class ImplementLinkedList<O> {
-	private ElementList<?> _head;
-	private ElementList<?> _tail;
+	private ElementList _head;
+	private ElementList _tail;
 
 	// create
+	public ImplementLinkedList() {
+		System.out.println("Create List");
+	}
+
+	/**
+	 * Выводит весь список на консоль
+	 */
+	public void readList() {
+		ElementList node = _head;
+		while (node != null) {
+			System.out.println(node.getValue());
+			node = node.getNext();
+		}
+	}
+
+	/**
+	 * Вставляем значение в конец списка addLast()
+	 * 
+	 * @param value
+	 */
+
 	public void updateList(O value) {
-		ElementList node = new ElementList(value);
-		if (_head == null) {
-			return;
-		} else if (_head == _tail) {
-			_head = null;
-			_tail = null;
-			return;
-		}
-
-	}
-
-	public static void readList(ElementList element) {
-
-		while (element != null) {
-			System.out.println(element._value);
-			element = element._next;
-		}
-	}
-
-	public void deleteElementList(O value) {
 		ElementList node = new ElementList(value);
 		if (_head == null) {
 			_head = node;
 			_tail = node;
 		} else {
-			_tail._next = node;
+			_tail.setNext(node);
 			_tail = node;
 		}
-
 	}
 
-	public boolean isEmpty() {
+	/**
+	 * удаляет из списка переданное значение, первое найденное
+	 * 
+	 * @param value
+	 */
+	public void deleteElementList(O value) {
+		ElementList node = _head;
+		if (_head == null) {
+			return;
+		} else if (_head == _tail) {
+			_head = null;
+			_tail = null;
+		} else
+			while (node.getNext() != null) {
+				if (node.getNext().getValue() == value) {
+					if (_tail == node.getNext()) {
+						_tail = node;
+					}
+					node.setNext(node.getNext().getNext());
+					return;
+				}
+				node = node.getNext();
+			}
+	}
 
-		return _head._next == null;
+	public boolean isEmptyList() {
+		return _head.getNext() == null;
 
 	}
 }
